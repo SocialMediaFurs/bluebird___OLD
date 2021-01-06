95da5cd17d9e2caab4b7d8e9e6306d42d02c5282
Paul25995 <paul-koplin@web.de>
2021-01-06 16:20:24 +0100

    added primevue

--- src/main.js
+++ src/main.js
@@ -1,8 +1,20 @@
 import '@babel/polyfill'
 import 'mutationobserver-shim'
 import { createApp } from 'vue'
+import PrimeVue from 'primevue/config';
 import App from './App.vue'
 import router from './router'
 import store from './store'
 
-createApp(App).use(store).use(router).mount('#app')
+import 'primevue/resources/themes/saga-blue/theme.css';
+import 'primevue/resources/primevue.min.css';
+import 'primeicons/primeicons.css';
+
+const app = createApp(App);
+app.use(store);
+app.use(router);
+
+/* PrimeVue */
+app.use(PrimeVue, { ripple: true });
+
+app.mount('#app');
