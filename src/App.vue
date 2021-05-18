28373ec856ecdf7d3c3815744d2f71fc97260afc
Paul25995 <paul-koplin@web.de>
2021-05-18 19:52:58 +0200

    Better Mobile View

--- src/App.vue
+++ src/App.vue
@@ -4,28 +4,31 @@
       <hr>
     </span>
     <div class="menu">
-      <img src="../src/assets/menu.png" width="32" height="32">
+      <img src="../src/assets/menu.png" alt="Profilbild" width="32" height="32">
     </div>
   </div>
-  <div id="nav">
-    <img src="../src/assets/akuma.jpg" alt="Akuma">
-    <router-link to="/">Home</router-link>
-    <router-link to="/post">Post</router-link>
-    <router-link to="/profil">Profil</router-link>
-    <router-link to="/about">About</router-link>
+  <div class="profil">
+    <h1>Noataks Profil</h1>
+  </div>
+  <div class="nav">
+    <div>
+      <img src="../src/assets/akuma.jpg" alt="Akuma">
+    </div>
+    <span class="nav-mobile">
+      <router-link to="/">Home</router-link>
+      <router-link to="/post">Post</router-link>
+      <router-link to="/profil">Profil</router-link>
+      <router-link to="/about">About</router-link>
+    </span>
   </div>
   <div class="content">
     <router-view />
   </div>
+  <div class="footer"></div>
 </template>
 
 <style lang="scss">
-$brand: #547EBD;
-$secondary: #FFAA1D;//#493BA8;
-$accent: #2c3e50;
-$link: #fffdf1;
-$link-active: #42b983;
-$shadow: #00000066;
+@import "assets/variables/variables";
 
 * {
   font-family: Roboto,serif;
@@ -53,7 +56,7 @@ html, body {
 
   span {
     width: calc(100% - 32px);
-    margin: 8px 20px;
+    margin: $spacing-8 $spacing-20;
 
     hr {
       background-color: $link;
@@ -64,23 +67,17 @@ html, body {
   }
 
   .menu {
-    margin-right: 15px;
+    margin-right: $spacing-16;
   }
 }
 
-#nav {
+.nav {
   display: flex;
   flex-direction: column;
-  position: fixed;
-  top: 0;
-  bottom: 0;
-  right: 25px;
   margin: auto;
   padding: 30px;
   height: fit-content;
-  width: 300px;
-  border-radius: 20px;
-  background-color: $brand;
+  width: 90%;
 
   img {
     border-radius: 50%;
@@ -91,19 +88,66 @@ html, body {
   a {
     color: $link;
     margin: 10px 0;
-    border-bottom: 1px solid $link;
+    border-bottom: 0;
     text-decoration: none;
     text-align: left;
-    padding: 0 0 5px 20px;
-
+    padding: 6px 19px;
   }
   .router-link-exact-active {
-    color: $link-active;
-    border-bottom: 1px solid $link-active;
+
+    border-bottom: 2px solid $link;
+  }
+
+  &-mobile {
+    padding-bottom: $spacing-8;
+    border-bottom: 1px solid $link;
+    width: 100%;
+  }
+
+  @media (min-width: $breakpoint-tablet) {
+    display: flex;
+    flex-direction: column;
+    position: fixed;
+    top: 0;
+    bottom: 0;
+    right: 25px;
+    margin: 117px auto;
+    padding: 30px;
+    height: fit-content;
+    width: 300px;
+    border-radius: 20px;
+    background-color: $brand;
+
+    a {
+      color: $link;
+      margin: 10px 0;
+      border-bottom: 1px solid $link;
+      text-decoration: none;
+      text-align: left;
+      padding: 0 0 5px 20px;
+    }
+
+    .router-link-exact-active {
+      color: $link-active;
+      border-bottom: 1px solid $link-active;
+    }
+
+    &-mobile {
+      border-bottom: 0;
+      display: flex;
+      flex-direction: column;
+    }
   }
 }
 
 .content {
-  width: calc(100% - 300px - 25px);
+  width: 100%;
+
+  @media (min-width: $breakpoint-tablet) {
+    width: calc(100% - 300px - 25px);
+  }
+}
+.footer {
+  height: 20px;
 }
 </style>
