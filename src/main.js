3d9f7176792cf6f13651b6370c3397d2e02374f0
Paul25995 <paul-koplin@web.de>
2021-10-07 17:30:21 +0200

    add tailwind

--- src/main.js
+++ src/main.js
@@ -5,6 +5,7 @@ import PrimeVue from 'primevue/config';
 import App from './App.vue';
 import router from './router';
 import store from './store';
+import './index.css'
 
 import 'primevue/resources/themes/saga-blue/theme.css';
 import 'primevue/resources/primevue.min.css';
