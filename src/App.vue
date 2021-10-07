12be13442267c20d22f35a60edc0afa78123763b
Paul25995 <paul-koplin@web.de>
2021-10-07 14:38:04 +0200

    testUser button2 main

--- src/App.vue
+++ src/App.vue
@@ -25,6 +25,8 @@
       <p>Login</p>
       <VueScriptComponent script='<script async src="https://telegram.org/js/telegram-widget.js?15" data-telegram-login="bluebird_login_bot" data-size="large" data-onauth="onTelegramAuth(user)" type="application/javascript"></script>'></VueScriptComponent>
       <VueScriptComponent script='<script type="application/javascript">function onTelegramAuth(user) {this.tgUser = user; alert("Logged in as " + user.first_name + " " + user.last_name + " (" + user.id + (user.username ? ", @" + user.username : "") + ")");}</script>'></VueScriptComponent>
+      <p>Test Login 2</p>
+      <VueScriptComponent script='<script async src="https://telegram.org/js/telegram-widget.js?15" data-telegram-login="bluebird_login_bot" data-size="large" data-auth-url="https://bluebird-projekt.web.app/"></script>'></VueScriptComponent>
     </span>
     {{tgUser}}
   </div>
